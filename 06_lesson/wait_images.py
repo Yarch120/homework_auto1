7308ff689a684a540a71cecce97e6f1f21becd96
Yarch120 <yaroslav.chekashin@yandex.ru>
2026-03-03 09:29:52 +0300

    commit

--- 06_lesson/wait_images.py
+++ 06_lesson/wait_images.py
@@ -8,7 +8,6 @@ driver = webdriver.Chrome()
 
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-
 wait = WebDriverWait(driver, timeout=20)
 wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#landscape")))
 
